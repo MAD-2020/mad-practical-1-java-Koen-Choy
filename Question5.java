71eb7ec89e5e2878c6e0a4b43b641d210bb17820
Koen-Choy <64003030+Koen-Choy@users.noreply.github.com>
2020-04-24 16:02:54 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Question5
 {
@@ -27,6 +30,36 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    ArrayList<Integer> data = new ArrayList<>();
+    Scanner in = new Scanner(System.in);
     
-  }
+    System.out.print("Enter the number of integers in the array: "); /* Gets length of array */
+    int length = in.nextInt(); 
+                                      
+    for (int x = 0; x < length; x++) /* Inputs integers into array */
+    {
+      System.out.print((x+1) + ". Input integer: ");
+      int number = in.nextInt();
+      data.add(number);
+    }
+    
+    ArrayList<Integer> distinct = new ArrayList<>();
+    for (int i = 0; i < length; i++) /* Creates new array without repeated integers */
+    {
+      if (!distinct.contains(data.get(i)))
+      {
+        distinct.add(data.get(i));
+      }
+    }
+    
+    ArrayList<Integer> counts = new ArrayList<>(); /* Creates new array with the frequencies of the different integers */
+    for (int i = 0; i < distinct.size(); i++)
+    {
+      counts.add(Collections.frequency(data, distinct.get(i)));
+    }
+    int maxcount = Collections.max(counts); /* Gets highest frequency */
+    int mode = distinct.get(counts.indexOf(maxcount)); /* Gets the integer with the highest frequency */
+    System.out.println("Mode: " + mode); /* That integer is the mode */
+  }  
 }
+/*Note: This does not work if the array has more than 1 modes as it only gives the mode at the lower index */
